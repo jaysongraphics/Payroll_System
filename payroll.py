c387d6bbc82faf4fe7236e8802886724ff8afc96
jaysongraphics <jaysongraphics.com>
2022-11-15 18:46:01 -0500

    Fin :)

--- payroll.py
+++ payroll.py
@@ -12,7 +12,7 @@ print(emojize('''
               |  /---------------------\  |
               | |                       | |
               | |     Edward            | |
-              | |         Arias         | |
+              | |           Arias       | |
               | |                       | |
               | |                       | |
               |  \_____________________/  |
@@ -33,41 +33,57 @@ print(emojize('''
 Employee_Info = []
 employee_number = 1
 
-def User_Input(message):
+def User_Input_String(message):
   while True:
     try:
-       userInput = float(input(message))       
+        userInput = str(input(message))
+        if userInput.isalpha() == False:
+          print(emojize(':stop_sign: Please enter an alphabetical name! Try again. :stop_sign:'))
+          continue
+        elif (len(userInput) < 2):
+           print(emojize(':stop_sign: Name must be at lease two characters long! Try again. :stop_sign:'))
+           continue
+    except TypeError:
+        continue
+    else:
+       return userInput 
+
+def User_Input_Hours(message):
+  while True:
+    try:
+       userInput = float(input(message))  
+       if (userInput <= 0 or userInput >= 65):
+          print(emojize(':stop_sign: Please enter a number between 1 and 65. Try again. :stop_sign:')) 
+          continue   
     except ValueError:
-       print(emojize(':stop_sign: Not an integer! Try again. :stop_sign:'))
+       print(emojize(':stop_sign: Not a number! Try again. :stop_sign:'))
        continue
     else:
-       return userInput      
-          
-def User_Input_string(message):
+       return userInput   
+     
+def User_Input_Rate(message):
   while True:
-    userInput = input(message)
-    if(len(userInput) < 3):
-      print(emojize(':stop_sign: Name must be three characters long! Try again. :stop_sign:'))
-      continue
+    try:
+       userInput = float(input(message))  
+       if (userInput <= 14 or userInput >= 51):
+          print(emojize(':stop_sign: Minimum $15 or Max $50 wage was not met, try again. :stop_sign:')) 
+          continue  
+    except ValueError:
+       print(emojize(':stop_sign: Not a number! Try again. :stop_sign:'))
+       continue
     else:
-      return userInput 
+       return userInput   
 
 while len(Employee_Info) < 2: 
-    Employee_Name = User_Input_string(emojize(f':radio_button: Please enter your name employee #{employee_number}: ')).lower()
-    Pay_Rate = User_Input(emojize(f':radio_button: Please enter your pay rate/hr employee #{employee_number}: '))
-    Hours_Worked = User_Input(emojize(f':radio_button: Please enter how many hours you worked this week employee #{employee_number}: '))
+    Employee_Name = User_Input_String(emojize(f':radio_button: Please enter your name employee #{employee_number}: '))
+    Pay_Rate = User_Input_Rate(emojize(f':radio_button: Please enter your pay rate/hr employee #{employee_number}: '))
+    Hours_Worked = User_Input_Hours(emojize(f':radio_button: Please enter how many hours you worked this week employee #{employee_number}: '))
     Overtime = Pay_Rate * 1.5 
     
     if (Hours_Worked >= 40 and Hours_Worked <= 65):
       Regular_Pay = Hours_Worked * Pay_Rate
       OT_Pay = Regular_Pay + Overtime 
-      Gross_Pay = Regular_Pay + OT_Pay 
-    elif (Hours_Worked <= 0 or Hours_Worked >= 65):
-        print(emojize(':stop_sign: Please enter a number between 1 and 65. Please start again. :stop_sign:'))
-        continue
-    elif (Pay_Rate <= 15 or Pay_Rate >= 50):
-        print(emojize(':stop_sign: Minimum wage $15 or Max wage $50 was not met, try again. :stop_sign:'))
-        continue      
+      Gross_Pay = Regular_Pay + OT_Pay   
     else:   
       Regular_Pay = Hours_Worked * Pay_Rate
       Gross_Pay = Regular_Pay 
@@ -85,5 +101,5 @@ while len(Employee_Info) < 2:
     employee_number = employee_number + 1
 
 headers_table = 'Employee Name', 'Hours Worked', '$ Pay_Rate', '$ Regular Pay', '$ OT Pay', '$ Gross Pay', '$ Fed Tax', '$ State Tax', '$ FICA', '$ Net Pay'    
-print(emojize(':money_bag: Please see below this week payroll! :money_bag:'))
+print(emojize(':money_bag: Please see this week payroll below: :money_bag:'))
 print(tabulate(Employee_Info, headers=headers_table, tablefmt='fancy_grid'))
